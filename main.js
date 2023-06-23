45943fdd1ccb1db1ff0e6222cc064a4cd7b4152f
Sriman <srimanp201@gmail.com>
2023-06-22 23:17:16 -0700

    working

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+
+
 window.addEventListener('load', () => {
   const form = document.querySelector("#new-task-form");
   const input = document.querySelector("#new-task-input");
   const list_el = document.querySelector("#tasks");
   let tasks = [];
 
-  // Load tasks from local storage
   if (localStorage.getItem("tasks")) {
     tasks = JSON.parse(localStorage.getItem("tasks"));
     renderTasks();
@@ -21,6 +22,7 @@ window.addEventListener('load', () => {
     const newTask = {
       id: Date.now(),
       content: task,
+      completed: false,
     };
 
     tasks.push(newTask);
@@ -34,33 +36,49 @@ window.addEventListener('load', () => {
 
     // Render each task
     tasks.forEach(task => {
+    const completion_btn = document.createElement("button");
+    completion_btn.classList.add("completion-button");
+    const star_icon = document.createElement("span");
+    star_icon.classList.add("material-icons-outlined");
+    completion_btn.appendChild(star_icon);
+    completion_btn.addEventListener("click", () => {
+      toggleCompletion(task.id);
+    });
       const task_el = document.createElement("div");
       task_el.classList.add("task");
-
       const task_content_el = document.createElement("div");
       task_content_el.classList.add("content");
       task_content_el.textContent = task.content;
 
+      if (task.completed) {
+        task_el.classList.add("completed");
+      }
       const delete_btn = document.createElement("button");
       delete_btn.classList.add("delete-button");
       delete_btn.textContent = "Delete";
       delete_btn.addEventListener("click", () => {
         deleteTask(task.id);
       });
-
       const update_btn = document.createElement("button");
       update_btn.classList.add("update-button");
       update_btn.textContent = "Update";
       update_btn.addEventListener("click", () => {
         updateTask(task.id);
       });
-
+      task_el.appendChild(completion_btn);
       task_el.appendChild(task_content_el);
       task_el.appendChild(delete_btn);
       task_el.appendChild(update_btn);
       list_el.appendChild(task_el);
     });
   }
+
+  function toggleCompletion(taskId) {
+    const taskToUpdate = tasks.find(task => task.id === taskId);
+    taskToUpdate.completed = !taskToUpdate.completed;
+    renderTasks();
+  }
+
 
   function saveTasksToLocalStorage() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
